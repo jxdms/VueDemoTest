aac45a64b324cba879d898a8dbaf976983b64f02
jxdms <jxnudms2022@163.com>
2022-09-27 17:29:29 +0800

    add new file

---
+++ src/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1 align="center">欢迎登录</h1>
+    <table align="center">
+      <tr>
+        <td>用户名:<input type="text" v-model="username" /></td>
+      </tr>
+      <tr>
+        <td><input type="submit" v-on:click="doLogin()" v-bind:value="btnname" /> </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data:function(){
+    return {
+      username:"",
+      password:"",
+      btnname:"提交"
+    };
+  },
+  methods:{
+    doLogin(){
+      console.log("......")
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
